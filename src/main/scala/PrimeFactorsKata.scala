c31607171e5690663f82066794581c8eb7187afd
paty-oliveira <patriciia.mota@gmail.com>
2021-04-30 09:15:20 +0100

    Add implementation of PrimeFactorsKata

--- src/main/scala/PrimeFactorsKata.scala
+++ src/main/scala/PrimeFactorsKata.scala
@@ -1,3 +1,16 @@
 object PrimeFactorsKata {
 
+  def primeFactors(number: Int): List[Int] = {
+    if (number <= 1) return List()
+
+    def foo(number: Int, i: Int): List[Int] =
+      if (i * i > number) List(number)
+      else
+        number % i match {
+          case 0 => i :: foo(number / i, i)
+          case _ => foo(number, i + 1)
+        }
+
+    foo(number, 2)
+  }
 }
--- src/test/scala/PrimeFactorsKataTest.scala
+++ src/test/scala/PrimeFactorsKataTest.scala
@@ -2,4 +2,59 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class PrimeFactorsKataTest extends AnyFunSuite{
 
+  test("Should be a empty list for 1"){
+    val inputNumber = 1
+    val expectedResult = List()
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should be a List(2) for 2"){
+    val inputNumber = 2
+    val expectedResult = List(2)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should be a List(3) for 3"){
+    val inputNumber = 3
+    val expectedResult = List(3)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should be a List(2, 3) for 6"){
+    val inputNumber = 6
+    val expectedResult = List(2, 3)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should be a List(7) for 7"){
+    val inputNumber = 7
+    val expectedResult = List(7)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should be a List(2, 2, 2) for 8"){
+    val inputNumber = 8
+    val expectedResult = List(2, 2, 2)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should return a List(3, 23) for 69"){
+    val inputNumber = 69
+    val expectedResult = List(3, 23)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
+
+  test("Should return a List(2, 3, 29) for 174"){
+    val inputNumber = 174
+    val expectedResult = List(2, 3, 29)
+
+    assert(PrimeFactorsKata.primeFactors(inputNumber) == expectedResult)
+  }
 }
