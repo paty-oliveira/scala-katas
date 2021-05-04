fb3dd7be288f8116e80893de0a1735a2506836a2
paty-oliveira <patriciia.mota@gmail.com>
2021-05-04 17:13:21 +0100

    Improve classes organization of TennisGameKata

--- src/main/scala/TennisGame.scala
+++
@@ -1,7 +0,0 @@
-class TennisGame(val playerName1: Player, val playerName2: Player) {
-
-  val player1: Player = playerName1
-  val player2: Player = playerName2
-
-
-}
--- src/main/scala/TennisGameKata.scala
+++ src/main/scala/TennisGameKata.scala
@@ -1,2 +1,27 @@
 object TennisGameKata {
 }
+
+class Player (val name: String) {
+
+  val playerName: String = name
+  var score: Int = 0
+
+  def getName: String = {
+    this.playerName
+  }
+
+  def getPoints: Int = {
+    this.score
+  }
+
+  def winBall(): Unit = {
+    this.score+=1
+  }
+}
+
+class TennisGame(val playerName1: Player, val playerName2: Player) {
+
+  val player1: Player = playerName1
+  val player2: Player = playerName2
+
+}
