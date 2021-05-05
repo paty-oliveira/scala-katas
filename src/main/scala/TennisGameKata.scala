e69914a0143943c9347c19d155724228938d351f
paty-oliveira <patriciia.mota@gmail.com>
2021-05-05 17:28:58 +0100

    Add implementation of TennisGameKata

--- src/main/scala/TennisGameKata.scala
+++ src/main/scala/TennisGameKata.scala
@@ -1,4 +1,39 @@
-object TennisGameKata {
+class TennisGame(val playerName1: Player, val playerName2: Player) {
+
+  val player1: Player = playerName1
+  val player2: Player = playerName2
+  val pointsInformation: Map[Int, String] = Map(0 -> "love", 1 -> "fifteen", 2 -> "thirty", 3 -> "forty")
+
+  def score: (String, String) = {
+    (pointsInformation(this.player1.getPoints), pointsInformation(this.player2.getPoints))
+  }
+
+  def scoreCompare: String = {
+    val playersInformation: Map[String, Int] = Map(this.player1.name -> this.player1.getPoints,
+                                                  this.player2.name -> this.player2.getPoints)
+    val leadPlayer = playersInformation.maxBy(_._2)._1
+    var gameStatus = ""
+
+    if (this.player1.getPoints >= 3 && this.player2.getPoints >= 3) {
+      if (this.player1.getPoints == this.player2.getPoints) {
+        gameStatus = "deuce"
+      }
+      if (scoreDifference(this.player1.getPoints, this.player2.getPoints) >= 1) {
+        gameStatus = "advantage " + leadPlayer
+      }
+    }
+
+    if (this.player1.getPoints >= 4 || this.player2.getPoints >= 4) {
+      if (scoreDifference(this.player1.getPoints, this.player2.getPoints) >= 2) {
+        gameStatus = leadPlayer + " won"
+      }
+    }
+    gameStatus
+  }
+
+  def scoreDifference(player1Score: Int, player2Score: Int): Int ={
+    Math.abs(player1Score - player2Score)
+  }
 }
 
 class Player (val name: String) {
@@ -14,14 +49,7 @@ class Player (val name: String) {
     this.score
   }
 
-  def winBall(): Unit = {
+  def winBall: Unit = {
     this.score+=1
   }
-}
-
-class TennisGame(val playerName1: Player, val playerName2: Player) {
-
-  val player1: Player = playerName1
-  val player2: Player = playerName2
-
 }
--- src/test/scala/TennisGameKataTest.scala
+++ src/test/scala/TennisGameKataTest.scala
@@ -1,10 +1,11 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class TennisGameKataTest extends AnyFunSuite{
-  val player1 = new Player("Patricia")
-  val player2 = new Player("Tiago")
 
   test("Should return the name of the players"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+
     var expectedResult = "Patricia"
     assert(player1.getName == expectedResult)
 
@@ -13,13 +14,18 @@ class TennisGameKataTest extends AnyFunSuite{
   }
 
   test("Should return the number of points of players"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+
     val expectedResult = 0
     assert(player1.getPoints == expectedResult)
     assert(player2.getPoints == expectedResult)
   }
 
   test("Should return the score of player1"){
+    val player1 = new Player("Patricia")
     player1.winBall
+
     var expectedResult = 1
     assert(player1.getPoints == expectedResult)
 
@@ -29,16 +35,96 @@ class TennisGameKataTest extends AnyFunSuite{
   }
 
   test("Should return the score of player2"){
-    player2.winBall
+    val player2 = new Player("Tiago")
     var expectedResult = 1
-    assert(player2.getPoints == expectedResult)
 
     player2.winBall
+    assert(player2.getPoints == expectedResult)
+
     expectedResult = 2
+    player2.winBall
     assert(player2.getPoints == expectedResult)
 
-    player2.winBall
     expectedResult = 3
+    player2.winBall
     assert(player2.getPoints == expectedResult)
+  }
+
+  test("Should return the (love, love) as score of the game"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = ("love", "love")
+
+    assert(game.score == expectedResult)
+  }
+
+  test("Should return (love, fifteen) as score of the game"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = ("love", "fifteen")
+
+    player2.winBall
+    assert(game.score == expectedResult)
+  }
+
+  test("Should return (thirty, fifteen) as score of the game"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = ("thirty", "fifteen")
+
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    assert(game.score == expectedResult)
+  }
+
+  test("Should return (forty, love) as score of the game"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = ("forty", "love")
+
+    (1 to 3).foreach( x => player1.winBall)
+    assert(game.score == expectedResult)
+  }
+
+  test("Should return Deuce to describe if at least three points have been scored," +
+    "and the scores are equal"){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = "deuce"
+
+    (1 to 3).foreach(x => player1.winBall)
+    (1 to 3).foreach(x => player2.winBall)
+    assert(game.scoreCompare == expectedResult)
+  }
+
+  test("The player should have advantage when at least three points have been scored" +
+    "and has a difference of one or more point than his opponent"
+  ){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = "advantage Tiago"
+
+    (1 to 3).foreach(x => player1.winBall)
+    (1 to 4).foreach(x => player2.winBall)
+    assert(game.scoreCompare == expectedResult)
+  }
+
+  test("The game should be won by the first player to have won at least four points in " +
+    "total and with at least two points more than the opponent "){
+    val player1 = new Player("Patricia")
+    val player2 = new Player("Tiago")
+    val game = new TennisGame(player1, player2)
+    val expectedResult = "Patricia won"
+
+    (1 to 7).foreach(x => player1.winBall)
+    (1 to 2).foreach(x => player2.winBall)
+    assert(game.scoreCompare == expectedResult)
   }
 }
