56c5617b16c208fe18e302021e9085256a5fb584
paty-oliveira <patriciia.mota@gmail.com>
2021-05-06 08:46:27 +0100

    Add implementation of StringPermutationKata

--- src/main/scala/StringPermutationKata.scala
+++ src/main/scala/StringPermutationKata.scala
@@ -1,3 +1,9 @@
 object StringPermutationKata {
 
+  def permutation(text: String): List[String] = {
+    text match {
+      case text if text.isEmpty => List()
+      case text => text.permutations.toList
+    }
+  }
 }
--- src/test/scala/StringPermutationKataTest.scala
+++ src/test/scala/StringPermutationKataTest.scala
@@ -1,3 +1,46 @@
-class StringPermutationKataTest {
+import org.scalatest.funsuite.AnyFunSuite
 
+class StringPermutationKataTest extends AnyFunSuite{
+
+  test("Should return an empty list when the input is empty"){
+    val inputText = ""
+    val expectedResult = List()
+
+    assert(StringPermutationKata.permutation(inputText) == expectedResult)
+  }
+
+  test("The result has 6 permutations"){
+    val inputText = "ABC"
+    val expectedResult = 6
+
+    StringPermutationKata.permutation(inputText).sizeCompare(expectedResult)
+  }
+
+  test("Should return a list with all permutations of ABC"){
+    val inputText = "ABC"
+    val expectedResult = List("ABC", "ACB", "BAC", "BCA", "CAB", "CBA")
+
+    assert(StringPermutationKata.permutation(inputText) == expectedResult)
+  }
+
+  test("The result should contain permutation ACB"){
+    var inputText = "ABC"
+    val expectedResult = "ACB"
+    StringPermutationKata.permutation(inputText) contains expectedResult
+
+    inputText = "CBA"
+    StringPermutationKata.permutation(inputText) contains expectedResult
+  }
+
+  test("The result should contain permutation BBYP"){
+    val inputText = "PBYB"
+    val expectedResult = "BBYP"
+    StringPermutationKata.permutation(inputText) contains expectedResult
+  }
+
+  test("The result should contain permutation 0099"){
+    val inputText = "0909"
+    val expectedResult = "0099"
+    StringPermutationKata.permutation(inputText) contains expectedResult
+  }
 }
