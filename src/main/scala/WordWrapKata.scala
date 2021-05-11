a8861298e4197140c66e16ff94a4bb7316e9c956
paty-oliveira <patriciia.mota@gmail.com>
2021-05-11 09:30:50 +0100

    Add implementation of WordWrapKata

--- src/main/scala/WordWrapKata.scala
+++ src/main/scala/WordWrapKata.scala
@@ -1,3 +1,12 @@
 object WordWrapKata {
 
+  implicit class StringImprovements(s: String) {
+
+    def wordWrap(maxLength: Int): String = {
+      s.split(" ").foldLeft(Array(""))( (out, in) => {
+        if ((out.last + " " + in).trim.length > maxLength) out :+ in
+        else out.updated(out.size - 1, out.last + " " + in)
+      }).mkString("n").trim
+    }
+  }
 }
--- src/test/scala/WordWrapKataTest.scala
+++ src/test/scala/WordWrapKataTest.scala
@@ -1,3 +1,32 @@
-class WordWrapKataTest {
+import org.scalatest.funsuite.AnyFunSuite
 
+class WordWrapKataTest extends AnyFunSuite{
+
+  test("Should return an empty string when no text is specified"){
+    val inputText = ""
+    val expectedResult = ""
+
+    assert(WordWrapKata.StringImprovements(inputText).wordWrap(0) == expectedResult)
+  }
+
+  test("Should return a single word"){
+    val inputText = "kata"
+    val expectedResult = "kata"
+
+    assert(WordWrapKata.StringImprovements(inputText).wordWrap(4) == expectedResult)
+  }
+
+  test("Should return wrapped text with small sample"){
+    val inputText = "This kata"
+    val expectedResult = "Thisnkata"
+
+    assert(WordWrapKata.StringImprovements(inputText).wordWrap(4) == expectedResult)
+  }
+
+  test("Should return a wrapped text with a big sample"){
+    val inputText = "This kata should be easy unless there are hidden, or not so hidden, obstacles. Let's start!"
+    val expectedResult = "This katanshould beneasy unlessnthere arenhidden, ornnot sonhidden,nobstacles.nLet's start!"
+
+    assert(WordWrapKata.StringImprovements(inputText).wordWrap(12) == expectedResult)
+  }
 }
