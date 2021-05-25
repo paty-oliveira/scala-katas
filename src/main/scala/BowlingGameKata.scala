1c599f445cf3e377d4585610bf82a78339776474
paty-oliveira <patriciia.mota@gmail.com>
2021-05-25 12:55:36 +0100

    Add implementation of BowlingGameKata

--- src/main/scala/BowlingGameKata.scala
+++ src/main/scala/BowlingGameKata.scala
@@ -1,3 +1,52 @@
-class BowlingGameKata {
+class BowlingGame{
 
+  var rolls: Array[Int] = new Array[Int](20)
+  var currentRoll: Int = 0
+
+  def roll(pins: Int): Unit = {
+    this.rolls(this.currentRoll) = pins
+    this.currentRoll += 1
+  }
+
+  def getScore: Int = {
+    var score = 0
+    var frameIndex = 0
+
+    while (frameIndex < 10){
+      if (isStrike(frameIndex)){
+        score += 10 + strikeBonus(frameIndex)
+        frameIndex += 2
+      }
+
+      else if (isSpare(frameIndex)){
+        score += 10 + spareBonus(frameIndex)
+        frameIndex += 2
+
+      }else {
+        score += sumBallsInFrame(frameIndex)
+        frameIndex += 2
+      }
+    }
+    score
+  }
+
+  def isSpare(frameIndex: Int): Boolean = {
+    this.rolls(frameIndex) + this.rolls(frameIndex + 1) == 10
+  }
+
+  def isStrike(frameIndex: Int): Boolean = {
+    this.rolls(frameIndex) == 10
+  }
+
+  def sumBallsInFrame(frameIndex: Int): Int = {
+    this.rolls(frameIndex) + this.rolls(frameIndex + 1)
+  }
+
+  def spareBonus(frameIndex: Int): Int = {
+    this.rolls(frameIndex + 2)
+  }
+
+  def strikeBonus(frameIndex: Int): Int = {
+    this.rolls(frameIndex + 1) + this.rolls(frameIndex + 2)
+  }
 }
--- src/test/scala/BowlingGameKataTest.scala
+++ src/test/scala/BowlingGameKataTest.scala
@@ -1,3 +1,36 @@
-class BowlingGameKataTest {
+import org.scalatest.funsuite.AnyFunSuite
 
+class BowlingGameKataTest extends AnyFunSuite {
+
+  test("Bowling game without any pin knocked with score 0"){
+    val game = new BowlingGame
+    val expectedScore = 0
+
+    (1 until 20).foreach(g => game.roll(0))
+
+    assert(game.getScore == expectedScore)
+  }
+
+
+  test("The score should be 16 emulating a spare"){
+    val game = new BowlingGame
+    val expectedScore = 16
+
+    game.roll(5)
+    game.roll(5) //spare
+    game.roll(3)
+
+    assert(game.getScore == expectedScore)
+  }
+
+  test("The score should be 24 emulating a strike") {
+    val game = new BowlingGame
+    val expectedScore = 10
+
+    game.roll(10)
+    game.roll(3)
+    game.roll(4)
+
+    assert(game.getScore == expectedScore)
+  }
 }
